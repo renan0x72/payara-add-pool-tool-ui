10870a40238ad97ffbdfe4fa5659dff311914e30
renanrrodrigues <renanrrodrigues@gmail.com>
2024-10-24 18:55:00 -0300

    salvar valores utilizados para preencher os campos. config.xml

--- src/main/java/br/com/payaraaddpooltool/FrmMain.java
+++ src/main/java/br/com/payaraaddpooltool/FrmMain.java
@@ -1,8 +1,17 @@
 package br.com.payaraaddpooltool;
 
-import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import org.w3c.dom.*;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Controller class for the main form.
@@ -49,6 +58,35 @@ public class FrmMain {
     private void initialize() {
         // Inicializando valores padrão
         titleLabel.setText("Add Pool to Payara");
+
+        // Carrega as configurações do arquivo config.xml se existir
+        File configFile = new File("config.xml");
+        if (configFile.exists()) {
+            loadConfig();
+        }
+    }
+
+    // Método para carregar as configurações do arquivo XML
+    private void loadConfig() {
+        try {
+            File configFile = new File("config.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(configFile);
+
+            doc.getDocumentElement().normalize();
+
+            payaraPathInput.setText(doc.getElementsByTagName("payaraPath").item(0).getTextContent());
+            ipInput.setText(doc.getElementsByTagName("ip").item(0).getTextContent());
+            payaraPortInput.setText(doc.getElementsByTagName("payaraPort").item(0).getTextContent());
+            portInput.setText(doc.getElementsByTagName("dbPort").item(0).getTextContent());
+            userInput.setText(doc.getElementsByTagName("user").item(0).getTextContent());
+            passwordInput.setText(doc.getElementsByTagName("password").item(0).getTextContent());
+            databaseChoiceBox.setText(doc.getElementsByTagName("database").item(0).getTextContent());
+            poolIdInput.setText(doc.getElementsByTagName("poolId").item(0).getTextContent());
+        } catch (Exception e) {
+            Logger.getLogger(FrmMain.class.getName()).log(Level.SEVERE, null, e);
+        }
     }
 
     @FXML
@@ -63,6 +101,9 @@ public class FrmMain {
         String password = passwordInput.getText();
         String database = databaseChoiceBox.getText();
         String poolId = poolIdInput.getText();
+
+        // Atualiza o arquivo de configuração
+        saveConfig();
 
         // Script 1: Monta o comando de criação do pool JDBC
         String command1 = String.format("%s/bin/asadmin create-jdbc-connection-pool --port %s --steadypoolsize=0 --maxpoolsize=50 "
@@ -98,5 +139,46 @@ public class FrmMain {
         //passwordInput.clear();
         //poolIdInput.clear();
         outputArea.clear();
+    }
+
+    // Método para salvar as configurações no arquivo XML
+    private void saveConfig() {
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.newDocument();
+
+            // Elemento raiz
+            Element rootElement = doc.createElement("config");
+            doc.appendChild(rootElement);
+
+            // Cria os elementos para cada campo e adiciona ao root
+            rootElement.appendChild(createElement(doc, "payaraPath", payaraPathInput.getText()));
+            rootElement.appendChild(createElement(doc, "ip", ipInput.getText()));
+            rootElement.appendChild(createElement(doc, "payaraPort", payaraPortInput.getText()));
+            rootElement.appendChild(createElement(doc, "dbPort", portInput.getText()));
+            rootElement.appendChild(createElement(doc, "user", userInput.getText()));
+            rootElement.appendChild(createElement(doc, "password", passwordInput.getText()));
+            rootElement.appendChild(createElement(doc, "database", databaseChoiceBox.getText()));
+            rootElement.appendChild(createElement(doc, "poolId", poolIdInput.getText()));
+
+            // Escreve o conteúdo no arquivo XML
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            StreamResult result = new StreamResult(new FileOutputStream("config.xml"));
+
+            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            transformer.transform(source, result);
+        } catch (Exception e) {
+            Logger.getLogger(FrmMain.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    // Método auxiliar para criar um elemento XML
+    private Element createElement(Document doc, String tagName, String value) {
+        Element element = doc.createElement(tagName);
+        element.appendChild(doc.createTextNode(value));
+        return element;
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module br.com.payaraaddpooltool {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.xml;       // Necessário para org.w3c.dom, javax.xml.parsers, javax.xml.transform, etc.
+    requires java.logging;   // Necessário para java.util.logging
 
     opens br.com.payaraaddpooltool to javafx.fxml;
     exports br.com.payaraaddpooltool;
